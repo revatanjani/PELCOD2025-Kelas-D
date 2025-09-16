afb32285d895fdb74e3bed4eb5f8769662bb2f82
reva <revatanjaniutm@gmail.com>
2025-09-16 20:34:45 +0700

    new file:   Tugas1.py

---
+++ Tugas1.py
@@ -0,0 +1,10 @@
+# Data diri
+nama =("Reva Tanjani")
+umur =("19 Tahun")
+tinggi =("170cm")
+angka_favorit =("78")
+
+print("nama", nama)
+print("umur", umur)
+print("tinggi", tinggi)
+print("angka favorit", angka_favorit)
