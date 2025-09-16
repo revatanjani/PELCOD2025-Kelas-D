6b8b5168cf8c2d8b49bab75bbb138d21a31e4d51
reva <revatanjaniutm@gmail.com>
2025-09-16 21:19:18 +0700

    new file:   Tugas2.py

---
+++ Tugas2.py
@@ -0,0 +1,16 @@
+# Hitung total harga belanja 
+print ("harga per pensil= 2000")
+harga_pensil= 2000
+print ("jumlah pembelian= 4 pensil")
+jumlah_pensil= 4
+total_harga_pensil= int(harga_pensil)*int(jumlah_pensil)
+print ("total harga pensil = ", total_harga_pensil)
+print ("harga per buku = 5000")
+harga_buku = 5000
+print ("jumlah pembelian=2 buku")
+jumlah_buku = 2
+total_harga_buku= int(harga_buku)*int(jumlah_buku)
+print ("total harga buku = ", total_harga_buku)
+total_pembelian = int(total_harga_buku)+int(total_harga_pensil)
+print ("total pembelian = ", total_pembelian)
+
